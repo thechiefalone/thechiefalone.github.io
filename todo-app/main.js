1b33bed6e3f73134d81e6d1259b51a9a9e478f8a
Alperen Ozkan <alperenozkan@gmail.com>
2023-01-20 20:48:12 +0100

    todo list application

---
+++ todo-app/main.js
@@ -0,0 +1,26 @@
+let todoBtn = document.querySelector(".todo__btn");
+let todoInput = document.querySelector(".todo__input");
+let todoList = document.querySelector(".todo__list");
+
+todoBtn.addEventListener("click", function(){
+
+    let todoItem = document.createElement("p");
+    todoItem.classList.add("todo__item");
+    todoItem.innerText = todoInput.value;
+
+    todoList.appendChild(todoItem);
+
+    if(todoInput.value === ""){
+        swal("Please enter a task !!!");
+    }else{
+        todoInput.value = "";
+    }
+
+    todoItem.addEventListener("click", function(){
+        todoItem.style.textDecoration="line-through";
+
+        todoItem.addEventListener("click", function(){
+            todoList.removeChild(todoItem);
+        });
+    });
+})
